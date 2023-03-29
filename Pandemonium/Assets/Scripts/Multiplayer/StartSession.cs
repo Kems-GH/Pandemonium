63284356e19099837f5e6042164fd6652ce0172d
Kémo <kemo.deschamps@outlook.fr>
2023-03-29 23:06:29 +0200

    add relay

--- Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
+++ Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
@@ -1,15 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using System;
 
 public class StartSession : MonoBehaviour
 {
+    private UnityTransport transport;
+    public TMPro.TMP_InputField joinCodeInputField;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if(MultiplayerParameters.isHost) NetworkManager.Singleton.StartHost();
-        if(MultiplayerParameters.isClient) NetworkManager.Singleton.StartClient();
+        transport = FindAnyObjectByType<UnityTransport>();
+
+    }
+
+    private async void Awake() {
+        await Authenticate();
+    }
+
+    private async System.Threading.Tasks.Task Authenticate() {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void createMultiplayerRelay(){
+        Allocation a = await RelayService.Instance.CreateAllocationAsync(2);
+        joinCodeInputField.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
+
+
+        transport.SetRelayServerData(a.RelayServer.IpV4, (ushort) a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public async void JoinSession()
+    {
+        try
+        {
+            JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(joinCodeInputField.text);
+
+            transport.SetClientRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error when trying to join multiplayer lobby " + e.Message);
+        }
     }
 
 }
