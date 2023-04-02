ce1fb6627926f672c36f18472aec001403e2b865
Kémo <kemo.deschamps@outlook.fr>
2023-04-03 01:49:51 +0200

    fix grab multiplayer

---
+++ Pandemonium/Assets/Scripts/GrabEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+public class GrabEvent : NetworkBehaviour
+{
+
+    public void Grab()
+    {
+        ChangeOwnershipServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void ChangeOwnershipServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        this.gameObject.GetComponent<NetworkObject>().ChangeOwnership(clientId);}
+}
