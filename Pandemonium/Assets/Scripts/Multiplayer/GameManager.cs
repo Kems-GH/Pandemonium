fb7a47c9624d822df4b5c1128a2ca4257f1d6a87
Kémo <kemo.deschamps@outlook.fr>
2023-04-12 22:07:35 +0200

    add keyboard

--- Pandemonium/Assets/Scripts/Multiplayer/GameManager.cs
+++ Pandemonium/Assets/Scripts/Multiplayer/GameManager.cs
@@ -20,7 +20,6 @@ public class GameManager : NetworkBehaviour {
 
     public override void OnNetworkSpawn() {
         SpawnPlayerServerRpc();
-        isSolo = false;
     }
 
     public bool IsSolo()
@@ -28,9 +27,11 @@ public class GameManager : NetworkBehaviour {
         return isSolo;
     }
 
-    public bool SetSolo(bool isSolo)
+    public GameManager SetSolo(bool isSolo)
     {
-        return isSolo;
+        this.isSolo = isSolo;
+
+        return this;
     }
 
     public void AddEnnemy()
@@ -38,9 +39,13 @@ public class GameManager : NetworkBehaviour {
         this.nbEnemy++;
     }
 
-    public void RemoveEnemy()
+    public bool RemoveEnemy()
     {
+        if (this.nbEnemy <= 0)
+            return false;
+
         this.nbEnemy--;
+        return true;
     }
 
     public int GetNbEnemy()
--- Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
+++ Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
@@ -10,7 +10,7 @@ using System;
 public class StartSession : MonoBehaviour
 {
     private UnityTransport transport;
-    public TMPro.TMP_Text joinCodeInputField;
+    public TMPro.TMP_Text joinCode;
     [SerializeField] private GameObject toDelete;
 
     [SerializeField] private GameObject btnHost;
@@ -19,10 +19,12 @@ public class StartSession : MonoBehaviour
 
     [SerializeField] private GameObject playerPrefab;
 
-
+    private TouchScreenKeyboard overlayKeyboard;
     void Start()
     {
+        string test = "";
         transport = FindAnyObjectByType<UnityTransport>();
+        
 
     }
 
@@ -36,45 +38,58 @@ public class StartSession : MonoBehaviour
     }
 
     public async void createMultiplayerRelay(){
+        OnStartSession();
         Allocation a = await RelayService.Instance.CreateAllocationAsync(2);
-        joinCodeInputField.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
+        joinCode.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
 
 
         transport.SetRelayServerData(a.RelayServer.IpV4, (ushort) a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
+        
         NetworkManager.Singleton.StartHost();
         Destroy(toDelete);
-        btnCancel.SetActive(true);
-        btnHost.SetActive(false);
-        btnClient.SetActive(false);
+
+        if(!NetworkManager.Singleton.IsHost) CancelSession();
+
     }
 
     public async void JoinSession()
     {
+        OnStartSession();
         try
         {
-            JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(joinCodeInputField.text);
+            JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(joinCode.text);
 
             transport.SetClientRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
-
             NetworkManager.Singleton.StartClient();
             Destroy(toDelete);
-            btnCancel.SetActive(true);
-            btnHost.SetActive(false);
-            btnClient.SetActive(false);
         }
         catch (Exception e)
         {
             Debug.LogError("Error when trying to join multiplayer lobby " + e.Message);
+            CancelSession();
         }
+    }
+    public void EnterCode()
+    {
+        overlayKeyboard = TouchScreenKeyboard.Open(joinCode.text, TouchScreenKeyboardType.Default);
+    }
+
+    private void OnStartSession()
+    {
+        btnCancel.SetActive(true);
+        btnHost.SetActive(false);
+        btnClient.SetActive(false);
+        GameManager.Instance.SetSolo(false);
     }
 
     public void CancelSession()
     {
-        NetworkManager.Singleton.DisconnectClient(NetworkManager.Singleton.LocalClientId);
+        NetworkManager.Singleton.Shutdown();
         
         GameManager.Instance.SetSolo(true);
         
         toDelete = Instantiate(playerPrefab);
+
         btnCancel.SetActive(false);
         btnHost.SetActive(true);
         btnClient.SetActive(true);
