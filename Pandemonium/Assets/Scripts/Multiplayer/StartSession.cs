954d77fc70840d4334c4d93252cded626d53d188
Kémo <kemo.deschamps@outlook.fr>
2023-04-19 19:30:12 +0200

    add prefab menu multi

--- Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
+++ Pandemonium/Assets/Scripts/Multiplayer/StartSession.cs
@@ -10,7 +10,7 @@ using System;
 public class StartSession : MonoBehaviour
 {
     private UnityTransport transport;
-    public TMPro.TMP_Text joinCodeInputField;
+    public TMPro.TMP_Text joinCode;
     [SerializeField] private GameObject toDelete;
 
     [SerializeField] private GameObject btnHost;
@@ -19,14 +19,19 @@ public class StartSession : MonoBehaviour
 
     [SerializeField] private GameObject playerPrefab;
 
+    private TouchScreenKeyboard overlayKeyboard;
+
+    public static StartSession instance;
 
     void Start()
     {
         transport = FindAnyObjectByType<UnityTransport>();
-
     }
 
     private async void Awake() {
+        if(instance == null) instance = this;
+        else  Debug.LogError("There is more than one StartSession in the scene");
+
         await Authenticate();
     }
 
@@ -36,45 +41,68 @@ public class StartSession : MonoBehaviour
     }
 
     public async void createMultiplayerRelay(){
+        OnStartSession();
         Allocation a = await RelayService.Instance.CreateAllocationAsync(2);
-        joinCodeInputField.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
+        joinCode.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
 
 
         transport.SetRelayServerData(a.RelayServer.IpV4, (ushort) a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
+        
         NetworkManager.Singleton.StartHost();
         Destroy(toDelete);
-        btnCancel.SetActive(true);
-        btnHost.SetActive(false);
-        btnClient.SetActive(false);
+
+        if(!NetworkManager.Singleton.IsHost) CancelSession();
+
     }
 
     public async void JoinSession()
     {
+        OnStartSession();
         try
         {
-            JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(joinCodeInputField.text);
+            JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(joinCode.text);
 
             transport.SetClientRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
-
             NetworkManager.Singleton.StartClient();
             Destroy(toDelete);
-            btnCancel.SetActive(true);
-            btnHost.SetActive(false);
-            btnClient.SetActive(false);
         }
         catch (Exception e)
         {
             Debug.LogError("Error when trying to join multiplayer lobby " + e.Message);
+            CancelSession();
         }
     }
-
+    public void EnterCode()
+    {
+        overlayKeyboard = TouchScreenKeyboard.Open(joinCode.text, TouchScreenKeyboardType.Default);
+    }
+    private void Update()
+    {
+        if(overlayKeyboard != null && GameManager.Instance.IsSolo())
+        {
+            joinCode.text = overlayKeyboard.text;
+            if (overlayKeyboard.status == TouchScreenKeyboard.Status.Done)
+            {
+                overlayKeyboard = null;
+                JoinSession();
+            }
+        }
+    }
+    private void OnStartSession()
+    {
+        btnCancel.SetActive(true);
+        btnHost.SetActive(false);
+        btnClient.SetActive(false);
+        GameManager.Instance.SetSolo(false);
+    }
     public void CancelSession()
     {
-        NetworkManager.Singleton.DisconnectClient(NetworkManager.Singleton.LocalClientId);
+        NetworkManager.Singleton.Shutdown();
         
         GameManager.Instance.SetSolo(true);
         
-        toDelete = Instantiate(playerPrefab);
+        this.toDelete = Instantiate(playerPrefab);
+
         btnCancel.SetActive(false);
         btnHost.SetActive(true);
         btnClient.SetActive(true);
