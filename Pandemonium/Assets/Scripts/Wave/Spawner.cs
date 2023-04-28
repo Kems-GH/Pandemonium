de89f5cd78e3ced478302cdcf3ea913957120bb6
Kemo Deschamps <kemo.deschamps@outlook.fr>
2023-04-28 17:02:44 +0200

    add spawn by  wave

--- Pandemonium/Assets/Scripts/Wave/Spawner.cs
+++ Pandemonium/Assets/Scripts/Wave/Spawner.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
-
-public class SpawnEnemy : NetworkBehaviour
+[System.Serializable]
+public class Spawner : NetworkBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
 
@@ -25,5 +25,28 @@ public class SpawnEnemy : NetworkBehaviour
             GameObject enemyGameObject = Instantiate(enemyPrefab, pos, transform.rotation);
             enemyGameObject.GetComponent<NetworkObject>().Spawn(true);
         }
+    }
+
+    public void Spawn(GameObject enemyPrefab)
+    {
+        if (!IsServer && !GameManager.Instance.IsSolo()) return;
+        if (GameManager.Instance.GetNbEnemy() < 5)
+        {
+            GameManager.Instance.AddEnnemy();
+            Vector3 randPos = Random.insideUnitSphere * 3;
+            // We create a new Vector3 to keep the y value
+            Vector3 pos = new Vector3(randPos.x + transform.position.x, transform.position.y, randPos.z + transform.position.z);
+
+            GameObject enemyGameObject = Instantiate(enemyPrefab, pos, transform.rotation);
+            enemyGameObject.GetComponent<NetworkObject>().Spawn(true);
+        }
+    }
+    public void Activate()
+    {
+        //TODO : Activate animation
+    }
+    public void Deactivate()
+    {
+        //TODO : Deactivate animation
     }
 }
---
+++ Pandemonium/Assets/Scripts/Wave/WaveManager.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections.Generic;
+using Pandemonium.Assets.Scripts;
+using System.Collections;
+
+public class WaveManager : NetworkBehaviour
+{
+    [SerializeField] private List<Spawner> spawners;
+    [SerializeField] private List<Wave> waves;
+
+    private int currentWave = 0;
+    private int nbWave = 0;
+    public static WaveManager Instance;
+
+    private void Awake() 
+    {
+        if (Instance == null) Instance = this;
+        else Debug.LogError("Multiple WaveManager in scene");
+        nbWave = waves.Count;
+    }
+
+    private void Start() {
+
+    }
+
+    private void StartWave()
+    {
+        if (!IsServer && !GameManager.Instance.IsSolo()) return;
+
+        if (currentWave < nbWave)
+        {
+            Wave wave = waves[currentWave];
+            StartCoroutine(SpawnEnemy(wave));
+            currentWave++;
+        }
+    }
+
+    private IEnumerator SpawnEnemy(Wave wave)
+    {
+        if (!IsServer && !GameManager.Instance.IsSolo()) yield break;
+
+        Spawner[] ActifSpawner = new Spawner[waves[currentWave].nbSpawner];
+
+        // Randomize spawner and select the number of spawner needed
+        Tools.ShuffleList(spawners);
+        spawners.CopyTo(0, ActifSpawner, 0, wave.nbSpawner < spawners.Count ? wave.nbSpawner : spawners.Count);
+
+        // Activate spawner
+        for(int i = 0; i < ActifSpawner.Length; i++)
+        {
+            ActifSpawner[i].Activate();
+        }
+        while(wave.nbEnemy > 0)
+        {
+            yield return new WaitForSeconds(wave.spawnRate);
+            // Spawn enemy on spawner
+            int nbEnemy = Mathf.Min(Random.Range(wave.minNbSpawn, wave.maxNbSpawn), wave.nbEnemy);
+            foreach (Spawner spawner in ActifSpawner)
+            {
+                for (int i = 0; i < nbEnemy; i++)
+                {
+                    int rand = Random.Range(0, 100);
+                    if (rand < wave.skeletonProba)
+                    {
+                        spawner.Spawn(GameManager.Instance.GetSkeletonPrefab());
+                    }
+                    else
+                    {
+                        spawner.Spawn(GameManager.Instance.GetSkeletonPrefab());
+                    }
+                }
+            }
+            wave.nbEnemy -= nbEnemy;
+        }
+        InvokeRepeating(nameof(CheckWaveFinished), 1f, 1f);
+    }
+
+    private void CheckWaveFinished()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            CancelInvoke(nameof(CheckWaveFinished));
+            EndWave();
+        }
+    }
+
+    private void EndWave()
+    {
+        if (!IsServer && !GameManager.Instance.IsSolo()) return;
+
+        foreach (Spawner spawner in spawners)
+        {
+            spawner.Deactivate();
+        }
+    }
+}
