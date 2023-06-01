0dd9ed1ef8cbeb7dd71862a16d1a827520e54e67
Arthur Ponty <arthur.ponty@epitech.eu>
2023-06-01 17:10:15 +0200

    Add menu Option interaction - Change mode deplacement and exit game

---
+++ Pandemonium/Assets/Scripts/Interfaces/ChangeModeDeplacement.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+
+public class ChangeModeDeplacement : MonoBehaviour
+{
+
+    private GameObject[] teleportInteractors;
+    private GameObject slideInteractor;
+    private TMP_Text textModeDeplacement;
+
+    private const string TAG_TEXT_MODE_DEPLACEMENT = "TextDeplacement";
+    private const string TAG_TELEPORT_INTERACTOR = "TeleportInteractor";
+    private const string TAG_SLIDE_INTERACTOR = "SlideInteractor";
+
+
+    private void Awake()
+    {
+        textModeDeplacement = GameObject.FindGameObjectWithTag(TAG_TEXT_MODE_DEPLACEMENT).GetComponent<TMP_Text>();
+        teleportInteractors = GameObject.FindGameObjectsWithTag(TAG_TELEPORT_INTERACTOR);
+        slideInteractor = GameObject.FindGameObjectWithTag(TAG_SLIDE_INTERACTOR);
+
+        MenuOption menuOption = FindAnyObjectByType<MenuOption>();
+        menuOption.ChangeModeHandler += ChangeMode;
+    }
+
+    public void ChangeMode(int mode)
+    {
+        ChangeVisualMode(mode);
+        if (mode == 0)
+        {
+            DeactivateSlide();
+            ActivateTeleport();
+        }
+        else if(mode == 1)
+        {
+            DeactivateTeleport();
+            ActivateSlide();
+        }
+    }
+
+    private void ChangeVisualMode(int mode)
+    {
+        if (mode == 0) textModeDeplacement.text = "Téléportation";
+        if (mode == 1) textModeDeplacement.text = "Glisser";
+    }
+
+    private void DeactivateTeleport()
+    {
+        foreach (GameObject interactor in teleportInteractors)
+        {
+            interactor.SetActive(false);
+        }
+    }
+
+    private void DeactivateSlide()
+    {
+        slideInteractor.GetComponent<SimpleCapsuleWithStickMovement>().enabled = false;
+    }
+
+    private void ActivateTeleport()
+    {
+        foreach (GameObject interactor in teleportInteractors)
+        {
+            interactor.SetActive(true);
+        }
+    }
+
+    private void ActivateSlide()
+    {
+        slideInteractor.GetComponent<SimpleCapsuleWithStickMovement>().enabled = true;
+    }
+}
---
+++ Pandemonium/Assets/Scripts/Interfaces/MenuOption.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class MenuOption : MonoBehaviour
+{
+    public delegate void ChangeModeEventHandler(int mode);
+
+    public event ChangeModeEventHandler ChangeModeHandler;
+
+    private int mode = 0;
+    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeModeDeplacement()
+    {
+        if(mode == 0) mode++;
+        else if(mode == 1) mode--;
+
+        ChangeModeHandler?.Invoke(mode);
+    }
+}
