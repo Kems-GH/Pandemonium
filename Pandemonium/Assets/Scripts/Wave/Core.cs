f38f50e6c859499af286ea5b119bca435d4a0e41
Arthur Ponty <arthur.ponty@epitech.eu>
2023-06-08 11:26:34 +0200

    Recenter core and add script

--- Pandemonium/Assets/Scripts/Wave/Core.cs
+++ Pandemonium/Assets/Scripts/Wave/Core.cs
@@ -4,36 +4,18 @@ using TMPro;
 
 public class Core : NetworkBehaviour
 {
-    [SerializeField] private TMP_Text text;
-
     private NetworkVariable<int> health = new NetworkVariable<int>(100);
-
-    public override void OnNetworkSpawn()
-    {
-        health.OnValueChanged += (int oldValue, int newValue) => { UpdateText(); };
-    }
-
-    private void Start() 
-    {
-        UpdateText();
-    }
 
     public void TakeDamage(int damage)
     {
         if (!IsServer) return;
 
         health.Value -= damage;
-        UpdateText();
 
         if(health.Value <= 0)
         {
             WaveManager.Instance.Defeat();
         }
-    }
-
-    void UpdateText()
-    {
-        text.text = health.Value.ToString();
     }
 
     public void ResetHealth()
