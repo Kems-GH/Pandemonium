72e21928a7acf129272b1d3ef7b4636e84d0db62
LouisRochotte <louis.rochotte@epitech.eu>
2023-07-07 12:42:11 +0200

    modif script throwableObject TriggerParticles

--- Pandemonium/Assets/Scripts/ParticleEffect/TriggerParticles.cs
+++ Pandemonium/Assets/Scripts/ParticleEffect/TriggerParticles.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticlTrigger : MonoBehaviour
+public class TriggerParticles : MonoBehaviour 
 {
     private ParticleSystem[] ps;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(WaitToDestroy());
+    }
+
+    IEnumerator WaitToDestroy()
+    {
         ps = this.gameObject.GetComponentsInChildren<ParticleSystem>();
         foreach (ParticleSystem particles in ps)
         {
             particles.Play();
         }
+        yield return new WaitForSeconds(2);
         Destroy(this.gameObject);
     }
 }
--- Pandemonium/Assets/Scripts/Props/ThrowableObjects.cs
+++ Pandemonium/Assets/Scripts/Props/ThrowableObjects.cs
@@ -1,24 +1,22 @@
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ThrowableObjects : GrabEvent
 {
     private Rigidbody rb;
-    private GameObject props_destruction;
-    
+    public GameObject Props_Destruction;
 
     private void OnTriggerEnter(Collider other)
     {
         if(!IsServer) return;
         if(this.rb == null) this.rb = this.gameObject.GetComponent<Rigidbody>();
         Enemy enemy = other.gameObject.GetComponent<Enemy>();
-        // if(enemy == null) return;
+        if(enemy == null) return;
         Debug.Log("Enemy hit : " + this.rb.velocity.magnitude );
         if(this.rb.velocity.magnitude > 5f)
         {
             enemy.TakeDamage(1);
-            // props_destruction = GameObject.Find("Props_Destruction");
-            GameObject particle = Instantiate<GameObject>(props_destruction);
-            if(IsServer) particle.GetComponent<NetworkObject>().Spawn(true);
+            Instantiate(Props_Destruction, transform.position, transform.rotation);
             Props_Destruction.transform.position = this.transform.position;
             Destroy(this.gameObject);
         }
